3b2c823a831d9879823420a12708e69d259c53fe
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-10-18 17:37:44 -0300

    Criado address,o usuário pode criar e buscar um endereço

--- api/graphql/resolvers/index.js
+++ api/graphql/resolvers/index.js
@@ -1,6 +1,7 @@
 import { mergeResolvers } from "@graphql-tools/merge";
 import userResolver from "./user.resolver.js";
+import addressResolver from "./address.resolver.js";
 
-const mergedResolver = mergeResolvers(userResolver);
+const mergedResolver = mergeResolvers([userResolver, addressResolver]);
 
 export default mergedResolver;
--- api/graphql/typeDefs/index.js
+++ api/graphql/typeDefs/index.js
@@ -1,6 +1,7 @@
 import { mergeTypeDefs } from "@graphql-tools/merge";
 import userTypeDef from "./user.typeDef.js";
+import addressTypeDef from "./address.typeDef.js";
 
-const mergedTypeDefs = mergeTypeDefs(userTypeDef);
+const mergedTypeDefs = mergeTypeDefs([userTypeDef, addressTypeDef]);
 
 export default mergedTypeDefs;
