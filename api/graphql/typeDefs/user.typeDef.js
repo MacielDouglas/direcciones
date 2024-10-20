4b8e40478131e46962d7ba4ff33d098b34dd10ac
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-10-20 15:34:27 -0300

    Alterado usuário para método switch case

--- api/graphql/typeDefs/user.typeDef.js
+++ api/graphql/typeDefs/user.typeDef.js
@@ -1,5 +1,4 @@
 const userTypeDef = `#graphql
-
 type Comment {
     cardId: ID!       # ID do card associado
     text: String!     # Texto do comentário com limite de 250 caracteres
@@ -16,57 +15,71 @@ type User {
     isSS: Boolean!
     myCards: [String]
     myTotalCards: [String]
-    comments: [Comment]  # Alterado para ser uma lista de "Comment"
+    comments: [Comment] 
 }
 
 type Query {
-    getUser(id: ID!): User
-    loginUser(email: String!, password: String!): LoginResponse!
-    logoutUser: LogoutResponse!
+    user(action: String!, id:ID, email:String, password: String): UserResponse
+    # getUser(id: ID!): User
+    # loginUser(email: String!, password: String!): LoginResponse!
+    # logoutUser: LogoutResponse!
 }
 
 type Mutation {
-    createUser(user: NewUserInput!): User
-    deleteUser(id: ID!): DeleteUserResponse
-    updateUser(id: ID!, updateUserInput: UpdateUserInput!): UpdateUserResponse!
+    userMutation(action: String!, user: NewUserInput, id: ID, updateUserInput: UpdateUserInput): UserMutationResponse!
+    # createUser(user: NewUserInput!): User
+    # deleteUser(id: ID!): DeleteUserResponse
+    # updateUser(id: ID!, updateUserInput: UpdateUserInput!): UpdateUserResponse!
 }
 
-type DeleteUserResponse {
-    success: Boolean!
-    message: String!
+type UserResponse {
+    user: User
+    success: Boolean
+    message: String
 }
+
+type UserMutationResponse {
+    success: Boolean!
+    message: String
+    user: User
+}
+
+# type DeleteUserResponse {
+#     success: Boolean!
+#     message: String!
+# }
 
 input UpdateUserInput {
     name: String
     profilePicture: String
 }
 
-type UpdateUserResponse {
-    success: Boolean!
-    message: String
-    name: String
-    profilePicture: String
-    isAdmin: Boolean
+# type UpdateUserResponse {
+#     success: Boolean!
+#     message: String
+#     name: String
+#     profilePicture: String
+#     isAdmin: Boolean
 
-}
+# }
 
-type LoginResponse {
-    token: String!
-    id: ID!
-    name: String!
-    profilePicture: String!
-    isAdmin: Boolean!
-    group: String!
-    isSS: Boolean!
-    myCards: [String]
-    myTotalCards: [String]
-    comments: [Comment]
-}
+# type LoginResponse {
+#     token: String!
+#     id: ID!
+#     name: String!
+#     profilePicture: String!
+#     isAdmin: Boolean!
+#     group: String!
+#     isSS: Boolean!
+#     myCards: [String]
+#     myTotalCards: [String]
+#     comments: [Comment]
+# }
 
-type LogoutResponse {
-    success: Boolean!
-    message: String!
-}
+# type LogoutResponse {
+#     success: Boolean!
+#     message: String!
+# }
 
 input NewUserInput {
     name: String!
