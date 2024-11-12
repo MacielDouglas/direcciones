a2b0abf5be2078d376739e98f14d3077e7f9a860
MacielDouglas <douglasmaciel.dm@gmail.com>
2024-11-12 11:39:40 -0300

    ajuste para rodar vercel

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
+// api/index.js
+
 import express from "express";
 import cors from "cors";
 import { ApolloServer } from "@apollo/server";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import { expressMiddleware } from "@apollo/server/express4";
 import { makeExecutableSchema } from "@graphql-tools/schema";
-
 import { WebSocketServer } from "ws";
 import { useServer } from "graphql-ws/lib/use/ws";
-
 import http from "http";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 import mergedTypeDefs from "./graphql/typeDefs/index.js";
 import mergeResolvers from "./graphql/resolvers/index.js";
 
@@ -28,8 +29,9 @@ mongoose
     console.log("Erro de conexão com MongoDB: ", error.message);
   });
 
-const start = async () => {
-  const app = express();
+const app = express();
+
+const startServer = async () => {
   const httpServer = http.createServer(app);
 
   const wsServer = new WebSocketServer({
@@ -74,11 +76,108 @@ const start = async () => {
     })
   );
 
-  const PORT = 8000;
-
-  httpServer.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}/graphql`);
-  });
+  return httpServer;
 };
 
-start();
+// Condição para rodar em modo local
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 8000;
+  startServer().then((httpServer) => {
+    httpServer.listen(PORT, () => {
+      console.log(
+        `Servidor rodando localmente em http://localhost:${PORT}/graphql`
+      );
+    });
+  });
+}
+
+// Exporta para ser utilizado no Vercel
+export default async (req, res) => {
+  const httpServer = await startServer();
+  httpServer.emit("request", req, res);
+};
+
+// import express from "express";
+// import cors from "cors";
+// import { ApolloServer } from "@apollo/server";
+// import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+// import { expressMiddleware } from "@apollo/server/express4";
+// import { makeExecutableSchema } from "@graphql-tools/schema";
+
+// import { WebSocketServer } from "ws";
+// import { useServer } from "graphql-ws/lib/use/ws";
+
+// import http from "http";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import mergedTypeDefs from "./graphql/typeDefs/index.js";
+// import mergeResolvers from "./graphql/resolvers/index.js";
+
+// dotenv.config();
+
+// const MONGODB_URI = process.env.MONGO_DB;
+// console.log("Conectando ao MongoDB....");
+
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then(() => {
+//     console.log("Conectado ao MongoDB");
+//   })
+//   .catch((error) => {
+//     console.log("Erro de conexão com MongoDB: ", error.message);
+//   });
+
+// const start = async () => {
+//   const app = express();
+//   const httpServer = http.createServer(app);
+
+//   const wsServer = new WebSocketServer({
+//     server: httpServer,
+//     path: "/graphql",
+//   });
+
+//   const schema = makeExecutableSchema({
+//     typeDefs: mergedTypeDefs,
+//     resolvers: mergeResolvers,
+//   });
+
+//   const serverCleanup = useServer({ schema }, wsServer);
+
+//   const server = new ApolloServer({
+//     schema,
+//     plugins: [
+//       ApolloServerPluginDrainHttpServer({ httpServer }),
+//       {
+//         async serverWillStart() {
+//           return {
+//             async drainServer() {
+//               await serverCleanup.dispose();
+//             },
+//           };
+//         },
+//       },
+//     ],
+//   });
+
+//   await server.start();
+
+//   app.use(
+//     "/graphql",
+//     cors({
+//       origin: ["http://localhost:5173", "https://direcciones.vercel.app"],
+//       credentials: true,
+//     }),
+//     express.json(),
+//     expressMiddleware(server, {
+//       context: ({ req, res }) => ({ req, res }),
+//     })
+//   );
+
+//   const PORT = 8000;
+
+//   httpServer.listen(PORT, () => {
+//     console.log(`Servidor rodando em http://localhost:${PORT}/graphql`);
+//   });
+// };
+
+// start();
