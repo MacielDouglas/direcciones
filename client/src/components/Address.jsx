f6c150edb8eaf11c6df4072d4739246d5bf33ecd
MacielDouglas <douglasmaciel.dm@gmail.com>
2025-02-15 08:32:58 -0300

    ajustado o tratamento de erros no componente DesignateCardInput para garantir que pelo menos um ID de cartão seja fornecido, melhorando a robustez da aplicação.

--- client/src/components/Address.jsx
+++ client/src/components/Address.jsx
@@ -13,7 +13,7 @@ import {
   MdOutlineApartment,
 } from "react-icons/md";
 import { FaRoute } from "react-icons/fa6";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { calculateDistance } from "../constants/direccion";
 
 const createCustomIcon = (iconUrl) =>
@@ -29,7 +29,7 @@ const pinMapIcon = createCustomIcon("pinMap.svg");
 function Routing({ userLocation, destination }) {
   const map = useMap();
   const [route, setRoute] = useState(null);
-  let isMounted = true; // Verifica se o componente está montado
+  const [isMounted, setIsMounted] = useState(null);
 
   const fetchRoute = useCallback(async () => {
     if (!map || !userLocation || !destination) return;
@@ -50,29 +50,22 @@ function Routing({ userLocation, destination }) {
           polyline.addTo(map);
           setRoute(polyline);
         }
-        // }).addTo(map);
-        // setRoute(polyline);
       }
     } catch (error) {
-      console.error("Erro ao obter a rota:", error);
+      console.error("Error al obtener la ruta:", error);
     }
-  }, [map, userLocation, destination]);
+  }, [map, userLocation, destination, isMounted]);
 
   useEffect(() => {
     fetchRoute();
 
     return () => {
-      isMounted = false;
+      setIsMounted(false);
       if (route) {
         route.remove();
       }
     };
   }, [fetchRoute, route]);
-
-  // useEffect(() => {
-  //   fetchRoute();
-  //   return () => route && route.remove();
-  // }, [fetchRoute, route]);
 
   return null;
 }
@@ -82,8 +75,6 @@ function Address({ id }) {
   const address = addresses.find((address) => address.id === id);
   const navigate = useNavigate();
   const [userLocation, setUserLocation] = useState(null);
-
-  if (!address) return <p className="text-center">Endereço não encontrado.</p>;
 
   const {
     street,
@@ -108,21 +99,10 @@ function Address({ id }) {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         }),
-      (error) => console.error("Erro ao obter localização do usuário:", error)
+      (error) =>
+        console.error("Error al obtener la ubicación del usuario:", error)
     );
   }, []);
-
-  console.log(userLocation);
-
-  const distance =
-    userLocation && latitude && longitude
-      ? calculateDistance(
-          userLocation.lat,
-          userLocation.lng,
-          latitude,
-          longitude
-        ).toFixed(0)
-      : "N/A";
 
   const typeIcons = useMemo(
     () => ({
@@ -135,9 +115,21 @@ function Address({ id }) {
     []
   );
 
+  if (!address) return <p className="text-center">Endereço não encontrado.</p>;
+
+  const distance =
+    userLocation && latitude && longitude
+      ? calculateDistance(
+          userLocation.lat,
+          userLocation.lng,
+          latitude,
+          longitude
+        ).toFixed(0)
+      : "N/A";
+
   const openMap = () => {
     const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&origin=${userLocation?.lat},${userLocation?.lng}&destination=${latitude},${longitude}&travelmode=walking`;
-    const wazeUrl = `https://waze.com/ul?ll=${latitude},${longitude}&navigate=yes`;
+    // const wazeUrl = `https://waze.com/ul?ll=${latitude},${longitude}&navigate=yes`;
     const appleMapsUrl = `maps://maps.apple.com/?saddr=${userLocation?.lat},${userLocation?.lng}&daddr=${latitude},${longitude}&dirflg=w`;
 
     if (navigator.userAgent.match(/iPhone|iPad|Mac/i)) {
@@ -159,7 +151,7 @@ function Address({ id }) {
         }`}
       >
         <h2 className="text-xl font-medium text-center mb-4">
-          Informações da Direção
+          Información de la dirección
         </h2>
         {photo && (
           <img
@@ -173,7 +165,7 @@ function Address({ id }) {
             confirmed ? "text-green-600" : "text-red-600"
           }`}
         >
-          {confirmed ? "Direção confirmada" : "Necessita confirmar"}
+          {confirmed ? "Dirección confirmada" : "Necessita confirmación"}
         </p>
         <div className="flex flex-col items-center space-y-3 text-sm">
           <div className="flex justify-between items-center w-full">
@@ -187,10 +179,10 @@ function Address({ id }) {
               </p>
             </div>
           </div>
-          <p className="text-lg font-semibold text-start w-full">
-            Calle: {`${street}, ${number}`}
+          <p className="text-lg  text-start w-full">
+            Calle: <strong>{`${street}, ${number}`}</strong>
           </p>
-          <div className="flex justify-between w-full">
+          <div className="flex justify-between w-full ">
             <p>
               Barrio: <strong>{neighborhood}</strong>
             </p>
@@ -198,29 +190,24 @@ function Address({ id }) {
               Ciudad: <strong>{city}</strong>
             </p>
           </div>
-
           <button
             onClick={openMap}
-            className="bg-blue-500 text-white px-3 py-1 rounded-md mt-2"
+            className="bg-blue-500 text-white px-3 py-1 rounded-md"
           >
-            Ver no Mapa
+            ver en el mapa
           </button>
-
-          {/* <Link
-            to={`https://www.google.com/maps/dir/?api=1&origin=${userLocation?.lat},${userLocation?.lng}&destination=${latitude},${longitude}&travelmode=walking`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-blue-500 text-white px-3 py-1 rounded-md mt-2"
-          >
-            Ver no Mapa
-          </Link> */}
         </div>
-        {complement && <p className="mt-2 text-center">{complement}</p>}
+        {complement && (
+          <div className="flex flex-col  w-full text-center bg-white rounded-md gap-2 p-3 mt-3">
+            <p className="text-sm font-medium">Otras informaciones</p>
+            <p>{complement}</p>
+          </div>
+        )}
         <button
-          className="w-full bg-black text-white py-2 rounded-md mt-3"
+          className="w-full bg-black text-white py-2 rounded-md mt-4"
           onClick={handleEdit}
         >
-          Editar Direção
+          Editar dirección
         </button>
       </div>
       <div className="w-full h-72 rounded-md overflow-hidden">
@@ -238,7 +225,7 @@ function Address({ id }) {
               position={[userLocation.lat, userLocation.lng]}
               icon={personIcon}
             >
-              <Popup>Você está aqui!</Popup>
+              <Popup>¡Usted está aquí!</Popup>
             </Marker>
           )}
           {userLocation && (
@@ -263,258 +250,3 @@ Routing.propTypes = {
 Address.propTypes = {
   id: PropTypes.string.isRequired,
 };
-
-// import { useMemo, useState, useEffect, useCallback } from "react";
-// import { useSelector } from "react-redux";
-// import PropTypes from "prop-types";
-// import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
-// import "leaflet-routing-machine";
-// import L from "leaflet";
-// import {
-//   MdHouse,
-//   MdRestaurant,
-//   MdHotel,
-//   MdOutlineStorefront,
-//   MdOutlineApartment,
-// } from "react-icons/md";
-// import { Link, useNavigate } from "react-router-dom";
-
-// // Ícones personalizados para os marcadores
-// const createCustomIcon = (iconUrl) =>
-//   new L.Icon({
-//     iconUrl,
-//     iconSize: [25, 41],
-//     iconAnchor: [12, 41],
-//   });
-
-// const personIcon = createCustomIcon("person.svg");
-// const pinMapIcon = createCustomIcon("pinMap.svg");
-
-// // Componente de Roteamento
-// function Routing({ userLocation, destination }) {
-//   const map = useMap();
-//   const [route, setRoute] = useState(null);
-//   let isMounted = true; // Verifica se o componente está montado
-
-//   const fetchRoute = useCallback(async () => {
-//     if (!map || !userLocation || !destination) return;
-
-//     const url = `https://router.project-osrm.org/route/v1/walking/${userLocation.lng},${userLocation.lat};${destination.lng},${destination.lat}?overview=full&geometries=geojson`;
-
-//     try {
-//       const response = await fetch(url);
-//       const data = await response.json();
-
-//       if (data?.routes?.length && isMounted) {
-//         const coordinates = data.routes[0].geometry.coordinates;
-//         const latlngs = coordinates.map(([lng, lat]) => [lat, lng]);
-
-//         const polyline = L.polyline(latlngs, {
-//           color: "#6c63ff",
-//           weight: 4,
-//         });
-
-//         if (isMounted) {
-//           polyline.addTo(map);
-//           setRoute(polyline);
-//         }
-//       }
-//     } catch (error) {
-//       console.error("Erro ao obter a rota:", error);
-//     }
-//   }, [map, userLocation, destination]);
-
-//   useEffect(() => {
-//     fetchRoute();
-
-//     return () => {
-//       isMounted = false;
-//       if (route) {
-//         route.remove();
-//       }
-//     };
-//   }, [fetchRoute, route]);
-
-//   return null;
-// }
-
-// // Componente principal
-// function Address({ id }) {
-//   const addresses = useSelector((state) => state.addresses.addressesData);
-//   const address = addresses.find((address) => address.id === id);
-//   const navigate = useNavigate();
-
-//   const [userLocation, setUserLocation] = useState(null);
-
-//   if (!address) {
-//     return <p>Endereço não encontrado.</p>;
-//   }
-
-//   const {
-//     street,
-//     number,
-//     neighborhood,
-//     city,
-//     type,
-//     gps,
-//     complement,
-//     confirmed,
-//     photo,
-//   } = address;
-
-//   const [latitude, longitude] = useMemo(
-//     () => gps.split(", ").map((coord) => parseFloat(coord.trim())),
-//     [gps]
-//   );
-
-//   useEffect(() => {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           setUserLocation((prevLocation) => {
-//             const newLocation = {
-//               lat: position.coords.latitude,
-//               lng: position.coords.longitude,
-//             };
-//             return prevLocation?.lat === newLocation.lat &&
-//               prevLocation?.lng === newLocation.lng
-//               ? prevLocation
-//               : newLocation;
-//           });
-//         },
-//         (error) => {
-//           console.error("Erro ao obter localização do usuário:", error);
-//         }
-//       );
-//     }
-//   }, []);
-
-//   const typeIcons = useMemo(
-//     () => ({
-//       house: <MdHouse />,
-//       department: <MdOutlineApartment />,
-//       store: <MdOutlineStorefront />,
-//       restaurant: <MdRestaurant />,
-//       hotel: <MdHotel />,
-//     }),
-//     []
-//   );
-
-//   const handleEdit = () => {
-//     navigate(`/address?tab=update-address&id=${id}`);
-//   };
-
-//   return (
-//     // <div className="w-full   rounded-xl">
-//     <div className="w-screen p-4 border rounded shadow-md ">
-//       <div
-//         className={`${
-//           confirmed ? "bg-primary" : "bg-red-100"
-//         }  p-5 rounded-md drop-shadow-lg mb-4 space-y-3`}
-//       >
-//         <h2 className="text-xl font-medium text-center mb-4">
-//           Informaciones de la dirección
-//         </h2>
-//         <hr />
-//         {photo && (
-//           <div className="w-full ">
-//             <img
-//               src={photo}
-//               className="object-cover rounded-t-md w-full h-64"
-//               alt={`imagem de ${street}, ${number}`}
-//               onError={(e) => {
-//                 if (e.target.src !== "/direccioes_map.svg") {
-//                   e.target.src = "/direccioes_map.svg";
-//                 }
-//               }}
-//             />
-//           </div>
-//         )}
-//         <p className={`font-bold ${confirmed ? "" : "text-red-600"}`}>
-//           {confirmed ? "Dirección confirmada" : "Necesita confirmar"}
-//         </p>
-//         <div className="flex gap-5 justify-between">
-//           <span className="text-5xl self-center">{typeIcons[type]}</span>
-//           <div className="text-sm ">
-//             <p className="text-xl mb-2">
-//               Rua: <strong>{`${street}, ${number}`}</strong>
-//             </p>
-//             <p>
-//               Bairro: <strong>{neighborhood}</strong>
-//             </p>
-//             <p>
-//               Cidade: <strong>{city}</strong>
-//             </p>
-//           </div>
-//           <Link
-//             to={`https://www.google.com/maps/dir/?api=1&origin=${userLocation?.lat},${userLocation?.lng}&destination=${latitude},${longitude}&travelmode=walking`}
-//             target="_blank"
-//             rel="noopener noreferrer"
-//             className="text-4xl font-semibold self-center"
-//           >
-//             IR
-//           </Link>
-//         </div>
-
-//         {complement && (
-//           <div className="border-t border-gray-200 mt-2 pt-2">
-//             <p>{complement}</p>
-//           </div>
-//         )}
-//         <div className="flex justify-center">
-//           <button
-//             className="bg-gradient-to-b from-stone-800  to-secondary text-white px-4 py-2 rounded hover:from-black hover:to-secondary w-full"
-//             onClick={handleEdit}
-//           >
-//             editar la dirección
-//           </button>
-//         </div>
-//         {/* </div> */}
-
-//         <div>
-//           <MapContainer
-//             center={[latitude, longitude]}
-//             zoom={15}
-//             style={{ height: "300px", width: "100%" }}
-//             className="rounded border z-0"
-//           >
-//             <TileLayer
-//               url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-//               attribution='&copy; <a href="https://carto.com/">Carto</a> contributors & <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//             />
-//             <Marker position={[latitude, longitude]} icon={pinMapIcon}>
-//               <Popup>{`${street}, ${number}, ${neighborhood}, ${city}`}</Popup>
-//             </Marker>
-//             {userLocation && (
-//               <Marker
-//                 position={[userLocation.lat, userLocation.lng]}
-//                 icon={personIcon}
-//               >
-//                 <Popup>Você está aqui!</Popup>
-//               </Marker>
-//             )}
-//             {userLocation && (
-//               <Routing
-//                 userLocation={userLocation}
-//                 destination={{ lat: latitude, lng: longitude }}
-//               />
-//             )}
-//           </MapContainer>
-//         </div>
-//       </div>
-//     </div>
-//     // </div>
-//   );
-// }
-
-// export default Address;
-
-// Routing.propTypes = {
-//   userLocation: PropTypes.object,
-//   destination: PropTypes.object,
-// };
-
-// Address.propTypes = {
-//   id: PropTypes.string.isRequired,
-// };
