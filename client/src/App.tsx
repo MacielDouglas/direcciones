b68781535d264a8c243ae117938c17abb2a5f3fe
MacielDouglas <douglasmaciel.dm@gmail.com>
2025-06-10 09:30:42 -0300

    refactor: remover código comentado e limpar importações não utilizadas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-<<<<<<< HEAD
 import { ThemeProvider } from "./context/ThemeContext";
-=======
-import PublicOnlyRoute from "./components/private/PublicOnlyRoute";
-import PrivateRoute from "./components/private/PrivateRoute";
-import Header from "./components/layout/Header";
-import { useSelector } from "react-redux";
-import { selectGroup } from "./store/selectors/userSelectors";
-import Footer from "./components/layout/Footer";
->>>>>>> 9e791c53fd2f35d01515d84226868c7d2d04ecaa
 
 const App = () => {
-  const group = useSelector(selectGroup);
+  // const group = useSelector(selectGroup);
   return (
-<<<<<<< HEAD
     <ThemeProvider>
       <Router>
         <Routes>
@@ -24,24 +14,6 @@ const App = () => {
         </Routes>
       </Router>
     </ThemeProvider>
-=======
-    <Router>
-      {group && group !== "0" && <Header />}
-      <Routes>
-        <Route element={<PublicOnlyRoute />}>
-          <Route path="/login" element={<Login />} />
-        </Route>
-        <Route element={<PrivateRoute />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-      </Routes>
-      {group && group !== "0" && (
-        <>
-          <Footer />
-        </>
-      )}
-    </Router>
->>>>>>> 9e791c53fd2f35d01515d84226868c7d2d04ecaa
   );
 };
 
